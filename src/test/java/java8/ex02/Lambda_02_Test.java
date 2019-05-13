795dfb6d40af7da0db664803599150e67b5fb059
nava <diego.nava-saucedo@wanadoo.fr>
2019-05-13 11:59:12 +0200

    part 2 lambda

--- src/test/java/java8/ex02/Lambda_02_Test.java
+++ src/test/java/java8/ex02/Lambda_02_Test.java
@@ -5,7 +5,9 @@ import java8.data.Data;
 import java8.data.Person;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 /**
@@ -22,7 +24,11 @@ public class Lambda_02_Test {
     // tag::map[]
     private List<Account> map(List<Person> personList, PersonToAccountMapper mapper) {
         // TODO implémenter la méthode pour transformer une liste de personnes en liste de comptes
-        return null;
+    	List<Account> newList = new ArrayList<Account>();
+    	for(Person person : personList) {
+    		newList.add(mapper.map(person));
+    	}
+    	return newList;
     }
     // end::map[]
 
@@ -35,7 +41,14 @@ public class Lambda_02_Test {
 
         // TODO transformer la liste de personnes en liste de comptes
         // TODO tous les objets comptes ont un solde à 100 par défaut
-        List<Account> result = map(personList, null);
+        List<Account> result = map(personList, 
+        		person ->  
+        {
+        			Account defaultAccount = new Account();
+        			defaultAccount.setOwner(person);
+        			defaultAccount.setBalance(100);
+        			return defaultAccount;
+        });
 
         assert result.size() == personList.size();
         for (Account account : result) {
@@ -52,7 +65,7 @@ public class Lambda_02_Test {
         List<Person> personList = Data.buildPersonList(100);
 
         // TODO transformer la liste de personnes en liste de prénoms
-        List<String> result = null;
+        List<String> result = personList.stream().map(person -> person.getFirstname()).collect(Collectors.toList());
 
         assert result.size() == personList.size();
         for (String firstname : result) {
